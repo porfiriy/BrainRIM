7a6d3010c3241f27fc3926803acf433d492c287e
porfiriy <porf2003@gmail.com>
2023-03-16 22:54:22 +0300

    file(connect)add comments

--- dataBase/connect.php
+++ dataBase/connect.php
@@ -7,6 +7,8 @@ $DBDATA = 'PROJECTDB';
 $CHARSET = 'utf8';
 $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC];
+//porf201v_db
+//o2gd13%l
 
 try{
    $pdo = new PDO("$DRIVER:host=$DBHOST;dbname=$DBDATA;charset=$CHARSET",$DBLOGIN,$DBPASS,$options
