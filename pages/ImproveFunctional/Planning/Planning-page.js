f228e61f3f9f9ceac6009ebbbf599c3011fcbe58
porfiriy <porf2003@gmail.com>
2023-05-04 22:34:37 +0300

    fix(planning)

--- pages/ImproveFunctional/Planning/Planning-page.js
+++ pages/ImproveFunctional/Planning/Planning-page.js
@@ -1,8 +1,18 @@
-const AddPlan = document.querySelectorAll('.add-plan');
-const NewPlanBody = document.querySelectorAll('.new-plan-body');
-const Back = document.querySelectorAll('.back');
-
+const AddPlan = document.querySelector('.add-plan');
+const NewPlanBody = document.querySelector('.new-plan-body');
+const Back = document.querySelector('.back');
+const todayDateValue = document.querySelector('.today-date');
 
 AddPlan.onclick = function () {//при нажатии открывается новый план
 	NewPlanBody.style = 'display: flex;';
 }
+
+var currentDate = new Date();
+var day = currentDate.getDate();
+var month = currentDate.toLocaleString('default', { month: 'long' });
+var year = currentDate.getFullYear();
+
+// Форматируем дату в нужный формат (дд-мм-гггг)
+var formattedDate = day + '-' + month + '-' + year;
+
+todayDateValue.innerHTML = formattedDate;
